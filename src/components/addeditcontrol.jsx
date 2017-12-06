f3e173bd7683368d7357ec76622c5318a3958df9
Yevhen Tsybulskyi <Y.Tsybulskyi@evision-software.com>
2017-12-06 17:30:18 +0100

    some code comments

--- src/components/addeditcontrol.jsx
+++ src/components/addeditcontrol.jsx
@@ -1,46 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import serializeForm from 'form-serialize'
 import SaveIcon from 'react-icons/lib/fa/floppy-o'
 
-class AddEditControl extends Component {
-    handleSubmit = (e) => {
+
+export default function AddEditControl({ post, savePost }) {
+    var handleSubmit = (e) => {
         e.preventDefault()
         const values = serializeForm(e.target, { hash: true })
-        if (this.props.savePost) {
-            this.props.savePost(values)
+        if (savePost) {
+            savePost(values)
         }
     }
 
-    render() {
-        const { post } = this.props
-
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit} className="create-contact-form">
-                    {post &&
-                        <div>
-                            <h3>Edit post</h3>
-                            <input type="hidden" name="id" defaultValue={post.id}/><br />
-                            <input type="text" name="title" className="editor-field" placeholder="Title" defaultValue={post.title} /><br />
-                            <input type="textarea" name="body" className="bodyArea" placeholder="Body" defaultValue={post.body} /><br />
-                            <input type="text" name="author" className="editor-field" placeholder="Author" defaultValue={post.author} /><br />
-                        </div>
-                    }
-                    {!post &&
-                        <div>
-                            <h3>Add post</h3>
-                            <input type="text" name="title" className="editor-field" placeholder="Title" /><br />
-                            <input type="textarea" name="body" className="bodyArea" placeholder="Body" /><br />
-                            <input type="text" name="author" className="editor-field" placeholder="Author" /><br />
-                        </div>
-                    }
-                    <button className='icon-btn'>
-                        <SaveIcon size={30} />
-                    </button>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit} className="create-contact-form">
+                {post &&
+                    <div>
+                        <h3>Edit post</h3>
+                        <input type="hidden" name="id" defaultValue={post.id} /><br />
+                        <input type="text" name="title" className="editor-field" placeholder="Title" defaultValue={post.title} /><br />
+                        <input type="textarea" name="body" className="bodyArea" placeholder="Body" defaultValue={post.body} /><br />
+                        <input type="text" name="author" className="editor-field" placeholder="Author" defaultValue={post.author} /><br />
+                    </div>
+                }
+                {!post &&
+                    <div>
+                        <h3>Add post</h3>
+                        <input type="text" name="title" className="editor-field" placeholder="Title" /><br />
+                        <input type="textarea" name="body" className="bodyArea" placeholder="Body" /><br />
+                        <input type="text" name="author" className="editor-field" placeholder="Author" /><br />
+                    </div>
+                }
+                <button className='icon-btn'>
+                    <SaveIcon size={30} />
+                </button>
+            </form>
+        </div>
+    )
 }
-
-export default AddEditControl
--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 import thunkMiddleware from 'redux-thunk'
 import { BrowserRouter } from 'react-router-dom'
+import logger from 'redux-logger'
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunkMiddleware)
+  applyMiddleware(thunkMiddleware, logger)
 )
 
 ReactDOM.render(<BrowserRouter><Provider store={store}><App /></Provider></BrowserRouter>, document.getElementById('root'));
